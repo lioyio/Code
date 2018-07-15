bff123d72f04d857d449952981f8b97180fd4fc1
lioyio <dtxliuyong@163.com>
2018-07-15 23:52:01 +0800

    增加全局鼠标弹起处理
    Signed-off-by: lioyio <dtxliuyong@163.com>

--- VueJs/Schedule/js/schedule.js
+++ VueJs/Schedule/js/schedule.js
@@ -48,21 +48,33 @@ Vue.component('schedule_list', {
 			Vue.set(this.rect, 'right', coords.x);
 			Vue.set(this.rect, 'bottom', coords.y);
 		},
-		dragOut(e){
-			Vue.set(this.rect,'show',false);
-		},
 		endDrag(){
+			Vue.set(this.rect,'show',false);
 			var flag = !this.data[this.selcolor][this.range.start.y][this.range.start.x]*1;
 			for(var i = this.adjustedRange.start.y;i <= this.adjustedRange.end.y;++i){
 				for(var j = this.adjustedRange.start.x;j <= this.adjustedRange.end.x;++j){
 					Vue.set(data[this.selcolor][i],j,flag);
-					//data[this.selcolor][i][j] = flag;
 				}
 			}
+			this.range.start.x = -1;
+			this.range.start.y = -1;
+			this.range.end.x = -1;
+			this.range.end.y = -1;
+		},
+		documentMouseUp(){
+			if(!this.rect.show)
+				return;
+			this.endDrag();
 		}
 	},
+	created:function(){
+		document.addEventListener("mouseup", this.documentMouseUp);
+	},
+	destroyed:function(){
+		document.removeEventListener("mouseup", this.documentMouseUp);
+	},
     template: '<div class="schedule_parent" :style="{position:`relative`,userSelect:`none`}">\
-					<table v-on="{mousemove:dragMove,mouseleave:dragOut,mouseup:endDrag}">\
+					<table @mousemove="dragMove" @mouseup.left="endDrag">\
 						<template v-for="(row,rowindex) in rows">\
 							<tr is="schedule_list_row" v-for="(color,colorindex) in colors"\
 							:color="color" :cols="cols" :cells="cells" :row="rowindex" :selcolor="selcolor"\
@@ -74,40 +86,35 @@ Vue.component('schedule_list', {
 });
 
 Vue.component('schedule_list_row', {
-    data: function () {
-        return {};
-    },
     props: ['color', 'cols', 'cells', 'data', 'selcolor', 'row', 'rect','range'],
     methods: {
-        triggle(e, col) {
-            //Vue.set(this.data, col, !this.data[col] * 1);
-        },
-        startDrag(e, col) {
+		startDrag(e, col) {
 			var coords = mouseCoords(e,document.getElementsByClassName('schedule_parent'));
 			Vue.set(this.rect, 'show', true);
 			Vue.set(this.rect, 'left', coords.x);
 			Vue.set(this.rect, 'top', coords.y);
 			Vue.set(this.rect, 'right', coords.x);
 			Vue.set(this.rect, 'bottom', coords.y);
-            console.log("start:row ", this.row, ",col ", col);
 			this.range.start.x = col;
 			this.range.start.y = this.row;
         },
         endDrag(e, col) {
-            Vue.set(this.rect, 'show', false);
 			Vue.set(this.rect, 'left', 0);
 			Vue.set(this.rect, 'top', 0);
 			Vue.set(this.rect, 'right', 0);
 			Vue.set(this.rect, 'bottom', 0);
-            console.log("start:row ", this.row, ",col ", col);
 			this.range.end.x = col;
 			this.range.end.y = this.row;
-        }
+        },
+		dragMove(col){
+			this.range.end.x = col;
+			this.range.end.y = this.row;
+		}
     },
-    template: '<tr><td @click="triggle($event,colindex)"\
-						@mouseleave.stop=""\
-						@mousedown="startDrag($event,colindex)"\
-						@mouseup="endDrag($event,colindex)"\
+    template: '<tr><td @click.right.prevent.stop=""\
+						@mousemove="dragMove(colindex)"\
+						@mousedown.left="startDrag($event,colindex)"\
+						@mouseup.left="endDrag($event,colindex)"\
 						v-for="(col,colindex) in cols"\
 						:style="{height:cells.height,width:cells.width,backgroundColor:data[colindex]?color:``}"></td></tr>'
 });
